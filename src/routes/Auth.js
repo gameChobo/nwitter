81baebc603c6282e9c2de00d09f2dddb1adfb502
ChoiC <gamerenderer@gmail.com>
2023-03-22 13:28:33 +0900

    04-2 fbaseLoginSetup

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,39 @@
-const Auth = () => <span>Auth</span>
+import { useState } from "react";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => {
+        const{
+            target: {name, value},
+        } = event;
+        if(name === "email"){
+            setEmail(value);
+        } else if(name == "password"){
+            setPassword(value);
+        }
+    }
+    
+    const onSubmit = (event) => {
+        event.preventDefault(); // Submit로 새로 고침 발생시 초기화 방지
+    }
+    
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="Email" required
+                    value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required
+                    value={password} onChange={onChange} />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+}
 
 export default Auth;
